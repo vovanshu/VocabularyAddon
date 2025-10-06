2dd1644c898d368ae8e286286cbb5d083a452313
Vladimir <vovanshu@gmail.com>
2025-10-06 14:05:10 +0300

    Delete data directory

--- data/scripts/uninstall.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$eventManager = $this->getServiceLocator()->get('EventManager');
-$args = $eventManager->prepareArgs(['module' => static::NAMESPACE, 'config' => $this->getConfig()]);
-$eventManager->trigger('module.uninstall', null, $args);
